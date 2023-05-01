9e42600b51d53ee4aedc56aa3c2e5097d2087572
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2023-05-01 23:22:09 +0530

    excel_to_pdf

---
+++ excel_to_pdf/README.md
@@ -0,0 +1,19 @@
+# Excel to PDF
+============
+
+Simple Python Program to convert from excel to PDF
+
+Folder Structure
+----------------
+├── data             <- Folder to save input datas
+|
+├── report           <- Folder where output saves
+|
+├── app.py           <- Main Python file
+└── requirements.txt <- Required Python Libraries
+----------------
+
+Used Modules and Methods
+------------------------
+# <a href:"https://reference.aspose.com/cells/python-java/asposecells.api/saveformat">asposecells.api.SaveFormat</a>
+Utility class containing constants. Represents the format in which the workbook is saved.
---
+++ excel_to_pdf/app.py
@@ -0,0 +1,48 @@
+"""
+Python program to convert Excel To PDF
+"""
+import jpype
+import os
+jpype.startJVM()
+from asposecells.api import Workbook, SaveFormat
+
+#Getting Path to cwd,data and report
+file_path = os.path.dirname(__file__)
+data_Path = f"{file_path}\\data"
+report_path = f"{file_path}\\report\\"
+
+def main():
+    #List comprehension for every file path
+    data_path = [ f.path for f in os.scandir(data_Path) ]
+    for data in data_path:
+        if "gitkeep" in data: continue #Getting error processing gitkeep as xls file
+        try:
+            #Loading the xls File to aspose Workbook
+            workbook = Workbook(data)
+            worksheet = workbook.getWorksheets().get(0)
+            worksheet.autoFitColumns()
+            #creating pdf file path to store the converted data
+            pdf_data = report_path + (os.path.basename(data).replace(".xls",".pdf"))
+            #class Workbook have save method with file path and saveformat as PDF
+            try:
+                os.remove(pdf_data)
+            except:
+                pass
+            workbook.save(f"{pdf_data}", SaveFormat.PDF)
+            print(f"Coverted from {os.path.basename(data)} to {os.path.basename(pdf_data)}")
+        except Exception as E:
+            print(E)
+            break
+    else:
+        """Run when there's no break in loop"""
+        print("Coversion Completed Successfully...")
+        jpype.shutdownJVM()
+        exit()
+
+    #If Error:
+    print("Some Error Occured")
+    jpype.shutdownJVM()
+
+
+if __name__ == "__main__":
+    main()
