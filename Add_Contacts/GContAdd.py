68bcff9a61bebe8a118991574c0e74c3500100b0
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2023-05-09 20:16:15 +0530

    generalized_path+3mail_version_added

--- Add_Contacts/GContAdd.py
+++ Add_Contacts/GContAdd.py
@@ -3,10 +3,12 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager #check for usage
+import csv
+import os
+from string import digits, punctuation
 import traceback
-from time import sleep
 
-numbers = ['0','1','2','3','4','5','6','7','8','9']
+file_path = os.path.dirname(__file__)
 
 #adding last name for understanding from which company this contacts are from
 COMPANY = "College"
@@ -16,9 +18,8 @@ try:
     profile = Options()
 
     ##maxim = webdriver.ChromeOptions()
-    profile.add_argument('user-data-dir=C:\\Users\\Thahir\\AppData\\Local\\Google\\Chrome\\User Data')
+    profile.add_argument(f'user-data-dir=C:\\Users\\{os.getlogin()}\\AppData\\Local\\Google\\Chrome\\User Data')
     profile.add_argument("--profile-directory=Profile 1")
-
     #initialise browser
     BrowserService = Service(executable_path='chromedriver.exe')
 
@@ -39,35 +40,68 @@ try:
 
     #looping through to get mails
     count = 0
-    mails = open('Add_Contacts\Mail.txt', 'r')
-    #contacts = open('Add_Contacts\contact.txt', 'w')
+    with open(file_path+'\Mail.txt', 'r') as file:
+        mails = file.readlines()
+    csv_file = open(file_path+'\contact.csv','a')
+    contacts_writer = csv.writer(csv_file)
     for mail in mails: #O(2n) because of removing numbers from emailid for only name
+        mail = mail.replace('\n','')
         #for those mail with no user --> email id not correct
-        if '"' not in mail:
-            continue
         driver.get('https://contacts.google.com/new')
-        email = mail.split('"')
-        namelist = email[1].split('@')
-        name = ""
-        for i in namelist[0]:
-            if i not in numbers:
-                name+=i
 
+        #Getting Mail and saveable Name From String
+        if '<' in mail:
+            email_list = mail.replace('>','<').split('<')
+            email = email_list[1]
+            namelist = email_list[0].split('@')
+            name = ""
+            for i in namelist[0]:
+                if i in digits:
+                    continue
+                if i in punctuation:
+                    name += " "
+                    continue
+                name += i
+            name = name.strip()
+        elif ',' in mail:
+            email_list = mail.split(',')
+            email = email_list[0]
+            namelist = email.split('@')
+            name = ""
+            for i in namelist[0]:
+                if i in digits:
+                    continue
+                if i in punctuation:
+                    name += " "
+                    continue
+                name += i
+            name = name.strip()
+        else:
+            email = mail
+            namelist = email.split('@')
+            name = ""
+            for i in namelist[0]:
+                if i in digits:
+                    continue
+                if i in punctuation:
+                    name += " "
+                    continue
+                name += i
+            name = name.strip()
         #entering mail in text boxes
         driver.find_element(By.XPATH, fnameXPath).send_keys(name)
         driver.find_element(By.XPATH, lnameXPath).send_keys(COMPANY)
-        driver.find_element(By.XPATH, emailXPath).send_keys(email[1])
+        driver.find_element(By.XPATH, emailXPath).send_keys(email)
         driver.find_element(By.XPATH, noteXPath).send_keys("This is Selenium Generated Contact")
         driver.find_element(By.XPATH, saveButton).click()
-        #contacts.write(f"Name: {name} Email: {email[1]}\n")
+        contacts_writer.writerow([name,email])
         count += 1
     
     #Quit Driver
     driver.quit()
 
     #close File
-    mails.close()
-    #contacts.close()
+    csv_file.close()
 
     print(f'Total {count} contacts Added')
 except:
